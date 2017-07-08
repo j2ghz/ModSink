d9d7c863fca0f70c3bbccb6b95d01c4fb4f4c994
J2ghz <j2.00ghz@gmail.com>
2017-07-08 21:52:13 +0200

    Add serilog

--- src/ModSink.WPF/App.xaml.cs
+++ src/ModSink.WPF/App.xaml.cs
@@ -6,22 +6,60 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Squirrel;
+using Serilog;
 
 namespace ModSink.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private ILogger log;
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            Helpers.ConsoleManager.Show();
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+            SetupLogging();
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Helpers.ConsoleManager.Hide();
+            }
+            this.log = Log.ForContext<App>();
+            log.Information("Starting ModSink ({version})", System.Reflection.Assembly.GetEntryAssembly().GetName().Version);
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                CheckUpdates();
+            }
+            base.OnStartup(e);
+        }
+
+        private void SetupLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.LiterateConsole(
+                        outputTemplate: "{Timestamp:HH:mm:ss} {Level:u3} [{SourceContext}] {Message:lj}{NewLine}{Exception}")
+                .WriteTo.RollingFile(
+                        "../Logs/{Date}.log",
+                        outputTemplate: "{Timestamp:o} [{Level:u3}] ({SourceContext}) {Message}{NewLine}{Exception}")
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug()
+                .CreateLogger();
+            Log.Information("Log initialized");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Helpers.ConsoleManager.Show();
+                Log.Fatal(args.ExceptionObject as Exception, nameof(AppDomain.CurrentDomain.UnhandledException));
+            };
+        }
+
+        private void CheckUpdates()
+        {
+            log.Information("Looking for updates");
             using (var mgr = new UpdateManager("https://modsink.j2ghz.com/release"))
             {
-                new Task(async () => await mgr.UpdateApp()).Start();
+                log.Information("Currently installed: {version}", mgr.CurrentlyInstalledVersion().ToString());
+                new Task(async () => await mgr.UpdateApp(p => log.Verbose("Checking updates {progress}", p)).ConfigureAwait(false)).Start();
             }
-
-            base.OnStartup(e);
         }
     }
 }
--- src/ModSink.WPF/Properties/AssemblyInfo.cs
+++ src/ModSink.WPF/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModSink.WPF")]
+[assembly: AssemblyProduct("ModSink")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
