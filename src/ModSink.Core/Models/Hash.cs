28466cf5c9abb147d80c7e7783645053f99ee653
J2ghz <j2.00ghz@gmail.com>
2017-07-16 22:12:09 +0200

    make hash struct

--- src/ModSink.Core/Models/Hash.cs
+++ src/ModSink.Core/Models/Hash.cs
@@ -1,11 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Diagnostics;
 
 namespace ModSink.Core.Models
 {
-    class Hash
+    [DebuggerDisplay("{value}")]
+    public struct Hash : IEquatable<Hash>, IFormattable
     {
-        public byte[] value
+        private readonly byte[] value;
+
+        public bool Equals(Hash other)
+        {
+            return this.value.Equals(other.value);
+        }
+
+        public Hash(byte[] value)
+        {
+            this.value = value;
+        }
+
+        public Hash(IEnumerable<byte> value)
+        {
+            this.value = value.ToArray();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.value.GetHashCode();
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return Convert.ToBase64String(this.value);
+        }
     }
 }
