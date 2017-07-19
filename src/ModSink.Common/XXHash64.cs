59ed57a49310a2fe7e13e84c2b89fc695390a432
J2ghz <j2.00ghz@gmail.com>
2017-07-20 00:31:50 +0200

    hashfunction

--- src/ModSink.Common/XXHash64.cs
+++ src/ModSink.Common/XXHash64.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using ModSink.Core.Models;
+﻿using ModSink.Core.Models.Local;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ModSink.Common
 {
-    [Serializable]
-    [DebuggerDisplay("{Value}")]
-    public struct XXHash64 : IHashValue, IEquatable<XXHash64>
+    public class XXHash64 : System.Data.HashFunction.xxHash, IHashFunction<XXHash64Value>
     {
-        public XXHash64(byte[] value) => this.Value = value;
-
-        public XXHash64(IEnumerable<byte> value) => this.Value = value.ToArray();
-
-        public byte[] Value { get; }
-
-        public bool Equals(XXHash64 other) => this.Value.Equals(other.Value);
-
-        public bool Equals(IHashValue other) => (other is XXHash64) && (Equals((XXHash64)other));
-
-        public override int GetHashCode() => this.Value.GetHashCode();
-
-        public override string ToString() => Convert.ToBase64String(this.Value);
+        async Task<XXHash64Value> IHashFunction<XXHash64Value>.ComputeHashAsync(Stream data, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var hash = await base.ComputeHashAsync(data);
+            return new XXHash64Value(hash);
+        }
     }
 }
---
+++ src/ModSink.Core/Models/Local/IHashFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ModSink.Core.Models.Local
+{
+    public interface IHashFunction<T>
+        where T : IHashValue
+    {
+        Task<T> ComputeHashAsync(Stream data, CancellationToken cancellationToken);
+    }
+}
