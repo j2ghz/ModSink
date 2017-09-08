16ee8d6c61eb7dbf6f3553b1bd60989b8ab55a5d
J2ghz <j2.00ghz@gmail.com>
2017-09-08 19:19:15 +0200

    Download and deserialize repo

--- src/ModSink.Common/Client/ClientManager.cs
+++ src/ModSink.Common/Client/ClientManager.cs
@@ -5,22 +5,28 @@ using System.Text;
 using ModSink.Core.Models.Repo;
 using System.Threading.Tasks;
 using System.Linq;
+using System.IO;
+using System.Reactive.Linq;
+using System.Runtime.Serialization;
 
 namespace ModSink.Common.Client
 {
     public class ClientManager : IClientManager
     {
-        public ClientManager(IDownloadManager downloadManager, ILocalRepoManager localRepoManager)
+        public ClientManager(IDownloadManager downloadManager, ILocalRepoManager localRepoManager, IDownloader downloader, IFormatter serializationFormatter)
         {
             this.DownloadManager = downloadManager;
             this.LocalRepoManager = localRepoManager;
+            this.Downloader = downloader;
+            this.SerializationFormatter = serializationFormatter;
         }
 
+        public IDownloader Downloader { get; }
         public IDownloadManager DownloadManager { get; }
         public ILocalRepoManager LocalRepoManager { get; }
         public ICollection<Modpack> Modpacks => this.Repos?.SelectMany(r => r.Modpacks).ToList();
-
         public ICollection<Repo> Repos { get; } = new List<Repo>();
+        public IFormatter SerializationFormatter { get; }
 
         public void DownloadMissingFiles(Modpack modpack)
         {
@@ -32,7 +38,7 @@ namespace ModSink.Common.Client
 
         public Uri GetDownloadUri(HashValue hash)
         {
-            foreach (var repo in Repos)
+            foreach (var repo in this.Repos)
             {
                 if (repo.Files.TryGetValue(hash, out Uri uri))
                 {
@@ -42,9 +48,13 @@ namespace ModSink.Common.Client
             throw new KeyNotFoundException($"Key {hash} was not found in a Files dictionary of any Repo");
         }
 
-        public Task<Repo> LoadRepo(Uri uri, IObserver<DownloadProgress> progress)
+        public async Task<Repo> LoadRepo(Uri uri, IObserver<DownloadProgress> progress)
         {
-            throw new NotImplementedException();
+            var stream = new MemoryStream();
+            var obs = this.Downloader.Download(uri, stream);
+            obs.Subscribe(progress);
+            await obs;
+            return this.SerializationFormatter.Deserialize(stream) as Repo;
         }
     }
 }
