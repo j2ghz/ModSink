2ad6373a07397cab5077edb52f9765a818b72900
J2ghz <j2.00ghz@gmail.com>
2017-11-21 09:46:25 +0100

    Fix tests

--- src/ModSink.Core/Models/Repo/FileSignature.cs
+++ src/ModSink.Core/Models/Repo/FileSignature.cs
@@ -18,11 +18,11 @@ namespace ModSink.Core.Models.Repo
             Length = Convert.ToUInt64(length);
         }
 
-        public HashValue Hash { get; }
+        public HashValue Hash { get; set; }
         /// <summary>
         /// The length of the file taken from <see cref="FileInfo"/>, in bytes.
         /// </summary>
-        public ulong Length { get; }
+        public ulong Length { get; set; }
 
 
         public bool Equals(FileSignature other)
