df000177835cbda51561c6890c828844bd15dca4
J2ghz <j2.00ghz@gmail.com>
2017-11-21 12:46:09 +0100

    Log binding exceptions

--- src/ModSink.WPF/App.xaml.cs
+++ src/ModSink.WPF/App.xaml.cs
@@ -130,10 +130,17 @@ namespace ModSink.WPF
                 log.ForContext(sender.GetType()).Fatal(args.Exception, "{exception}",
                     nameof(DispatcherUnhandledException));
             };
-            AppDomain.CurrentDomain.FirstChanceException += (sender, args) =>
+            //AppDomain.CurrentDomain.FirstChanceException += (sender, args) =>
+            //{
+            //    log.ForContext(sender.GetType()).Verbose(args.Exception, "FirstChanceException");
+            //};
+            PresentationTraceSources.Refresh();
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new RelayTraceListener(m =>
             {
-                log.ForContext(sender.GetType()).Verbose(args.Exception, "FirstChanceException");
-            };
+                log.ForContext(typeof(PresentationTraceSources)).Error(m);
+            }));
+            PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Warning | SourceLevels.Error;
+
         }
     }
 }
