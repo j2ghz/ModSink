1a8847fc94ae3075714668606ca0f5901d703cd5
J2ghz <j2.00ghz@gmail.com>
2017-11-22 10:12:31 +0100

    Add theme selection

--- src/ModSink.WPF/ViewModel/SettingsViewModel.cs
+++ src/ModSink.WPF/ViewModel/SettingsViewModel.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro;
 using ModSink.WPF.Model;
 using ReactiveUI;
 
@@ -14,6 +13,36 @@ namespace ModSink.WPF.ViewModel
         {
             Settings = settings;
         }
+
+        public ICollection<Accent> Accents { get; } = ThemeManager.Accents.ToList();
+
+        public Accent AccentSelected
+        {
+            get => ThemeManager.DetectAppStyle(Application.Current).Item2;
+            set
+            {
+                this.RaisePropertyChanging();
+                ThemeManager.ChangeAppStyle(Application.Current, value,
+                    ThemeManager.DetectAppStyle(Application.Current).Item1);
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public ICollection<AppTheme> Themes { get; } = ThemeManager.AppThemes.ToList();
+
+
+        public AppTheme ThemeSelected
+        {
+            get => ThemeManager.DetectAppStyle(Application.Current).Item1;
+            set
+            {
+                this.RaisePropertyChanging();
+                ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.DetectAppStyle(Application.Current).Item2,
+                    value);
+                this.RaisePropertyChanged();
+            }
+        }
+
 
         public SettingsModel Settings { get; }
     }
