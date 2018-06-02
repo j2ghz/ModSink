0c07c94162d9bab6bd60e8e442f8c31898d58172
J2ghz <j2.00ghz@gmail.com>
2018-06-02 13:28:54 +0200

    fix stream is disposed issue

--- src/ModSink.Common/HttpClientDownloader.cs
+++ src/ModSink.Common/HttpClientDownloader.cs
@@ -4,11 +4,9 @@ using System.Net.Http;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Threading.Tasks;
 using Fusillade;
 using Humanizer;
 using Humanizer.Bytes;
-using ModSink.Common.Client;
 using ModSink.Core.Client;
 using static ModSink.Core.Client.DownloadProgress;
 
@@ -18,7 +16,8 @@ namespace ModSink.Common
     {
         private readonly HttpClient client = new HttpClient(NetCache.UserInitiated);
 
-        public IConnectableObservable<DownloadProgress> Download(Uri source, Stream destination, ulong expectedLength = 0)
+        public IConnectableObservable<DownloadProgress> Download(Uri source, Stream destination,
+            ulong expectedLength = 0)
         {
             return Observable.Create<DownloadProgress>(async (observer, cancel) =>
             {
@@ -35,22 +34,16 @@ namespace ModSink.Common
                     report(ByteSize.FromBytes(0), ByteSize.FromBytes(0), TransferState.ReadingResponse);
                     response.EnsureSuccessStatusCode();
                     if (expectedLength != 0)
-                    {
                         if (response.Content.Headers.ContentLength > 0)
-                        {
                             if (Convert.ToUInt64(response.Content.Headers.ContentLength) != expectedLength)
-                            {
-                                throw new HttpRequestException($"Size of the download ({response.Content.Headers.ContentLength}) differed from expected ({expectedLength})");
-                            }
-                        }
-                    }
+                                throw new HttpRequestException(
+                                    $"Size of the download ({response.Content.Headers.ContentLength}) differed from expected ({expectedLength})");
                     var length = ByteSize.FromBytes(response.Content.Headers.ContentLength ?? 0);
-                    
+
                     report(length, ByteSize.FromBytes(0), TransferState.ReadingResponse);
 
                     var totalRead = 0;
                     using (var input = await response.Content.ReadAsStreamAsync())
-                    using (var output = destination)
                     {
                         var buffer = new byte[16 * 1024];
                         var read = 0;
@@ -59,7 +52,7 @@ namespace ModSink.Common
                         report(length, ByteSize.FromBytes(0), TransferState.Downloading);
                         while ((read = await input.ReadAsync(buffer, 0, buffer.Length, cancel)) > 0)
                         {
-                            output.Write(buffer, 0, read);
+                            destination.Write(buffer, 0, read);
                             totalRead += read;
                             report(length, totalRead.Bytes(), TransferState.Downloading);
                         }
