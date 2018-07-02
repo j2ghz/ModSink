ed2585601fdb00c92383d4816028cdac7a735a78
J2ghz <j2.00ghz@gmail.com>
2018-07-03 00:40:37 +0200

    HashValue test

---
+++ src/ModSink.Common.Tests/Models/Repo/HashValueTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using ModSink.Common.Models.Repo;
+using Xunit;
+
+namespace Modsink.Common.Tests.Models.Repo
+{
+    public class HashValueTests
+    {
+        [Fact]
+        public void CreateFromToStringResult()
+        {
+            var hash = new HashValue(new byte[] {0x99, 0xE9, 0xD8, 0x51, 0x37, 0xDB, 0x46, 0xEF});
+            var str = hash.ToString();
+            var result = new HashValue(str);
+            result.Should().BeEquivalentTo(hash);
+        }
+    }
+}
