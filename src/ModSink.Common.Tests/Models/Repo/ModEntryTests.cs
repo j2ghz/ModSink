79567f9cdc75cc1490b7f1bd02cc1476415acdc8
J2ghz <j2.00ghz@gmail.com>
2018-07-03 13:58:19 +0200

    Mod Entry test

---
+++ src/ModSink.Common.Tests/Models/Repo/ModEntryTests.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using FluentAssertions;
+using ModSink.Common.Models.Repo;
+using Xunit;
+
+namespace Modsink.Common.Tests.Models.Repo
+{
+    public class ModEntryTests
+    {
+        public static readonly Faker<ModEntry> ModEntryFaker =
+            new Faker<ModEntry>().StrictMode(true)
+                .RuleFor(m => m.Mod, ModTests.ModFaker.Generate())
+                .RuleForType(typeof(bool), f => f.Random.Bool());
+
+        [Fact]
+        public void HasValidFaker()
+        {
+            ModEntryFaker.AssertConfigurationIsValid();
+        }
+
+
+        [Fact]
+        public void IsSerializeable()
+        {
+            Assert.All(ModEntryFaker.Generate(5), modEntry => { modEntry.Should().BeBinarySerializable(); });
+        }
+    }
+}
