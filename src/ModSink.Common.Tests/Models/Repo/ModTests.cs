09b1dace1ca89dac0f7bb34abb460ce5ff32ca37
J2ghz <j2.00ghz@gmail.com>
2018-07-03 13:43:11 +0200

    Mod tests

---
+++ src/ModSink.Common.Tests/Models/Repo/ModTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Bogus;
+using FluentAssertions;
+using ModSink.Common.Models.Repo;
+using Xunit;
+
+namespace Modsink.Common.Tests.Models.Repo
+{
+    public class ModTests
+    {
+        public static readonly Faker<Mod> ModFaker =
+            new Faker<Mod>().StrictMode(true)
+                .RuleFor(m => m.Version, f => f.System.Semver())
+                .RuleFor(m => m.Name, f => f.System.FileName())
+                .RuleFor(m => m.Files,
+                    f => f.Make(10,
+                        () => new Tuple<Uri, FileSignature>(new Uri(Path.GetFullPath(f.System.FilePath())),
+                            FileSignatureTests.FileSignature)).ToDictionary(t => t.Item1, t => t.Item2));
+
+        [Fact]
+        public void HasValidFaker()
+        {
+            ModFaker.AssertConfigurationIsValid();
+        }
+
+
+        [Fact]
+        public void IsSerializeable()
+        {
+            Assert.All(ModFaker.Generate(5), mod => { mod.Should().BeBinarySerializable(); });
+        }
+    }
+}
