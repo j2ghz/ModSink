bdb1de6fdf31fe09a5456e11beaf64e863b8d00c
J2ghz <j2.00ghz@gmail.com>
2018-07-03 12:54:39 +0200

    FileSignature tests

--- src/ModSink.Common/Models/Repo/FileSignature.cs
+++ src/ModSink.Common/Models/Repo/FileSignature.cs
@@ -44,10 +44,5 @@ namespace ModSink.Common.Models.Repo
                 return (Hash.GetHashCode() * 397) ^ Length.GetHashCode();
             }
         }
-
-        public override string ToString()
-        {
-            return Hash.ToString();
-        }
     }
 }
