fb09496c9077782e8a51891e9458ccd54105b6e6
J2ghz <j2.00ghz@gmail.com>
2018-07-18 17:12:10 +0200

    DownloadView

---
+++ src/ModSink.WPF/View/DownloadView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ModSink.WPF.ViewModel;
+using ReactiveUI;
+
+namespace ModSink.WPF.View
+{
+    public partial class DownloadView : ReactiveUserControl<DownloadViewModel>
+    {
+        public DownloadView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.Name, v => v.TbName.Text).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.Speed, v => v.TbSpeed.Text).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.Downloaded, v => v.TbDownloaded.Text).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.Size, v => v.TbSize.Text).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.Progress, v => v.ProgressBar.Value).DisposeWith(d);
+
+            });
+        }
+    }
+}
