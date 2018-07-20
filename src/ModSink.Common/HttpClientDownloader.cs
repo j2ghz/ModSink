e96738b4fc15250d7d871d3635f446c29ddf921b
J2ghz <j2.00ghz@gmail.com>
2018-07-20 15:21:45 +0200

    dont cantch task asancelled

--- src/ModSink.Common/HttpClientDownloader.cs
+++ src/ModSink.Common/HttpClientDownloader.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -25,63 +24,56 @@ namespace ModSink.Common
         {
             return Observable.Create<DownloadProgress>(async (observer, cancel) =>
             {
+                var report = new Action<ByteSize, ByteSize, TransferState>((size, downloaded, state) =>
+                    observer.OnNext(new DownloadProgress(size, downloaded, state)));
+                //Get response
+                report(ByteSize.FromBytes(0), ByteSize.FromBytes(0), TransferState.AwaitingResponse);
+                var response = await client.GetAsync(source, HttpCompletionOption.ResponseHeadersRead,
+                    cancel);
+
+                //Read response
+                report(ByteSize.FromBytes(0), ByteSize.FromBytes(0), TransferState.ReadingResponse);
+
                 try
                 {
-                    var report = new Action<ByteSize, ByteSize, TransferState>((size, downloaded, state) =>
-                        observer.OnNext(new DownloadProgress(size, downloaded, state)));
-                    //Get response
-                    report(ByteSize.FromBytes(0), ByteSize.FromBytes(0), TransferState.AwaitingResponse);
-                    LogTo.Verbose("Sent request to {url}", source);
-                    var response = await client.GetAsync(source, HttpCompletionOption.ResponseHeadersRead,
-                        cancel);
-
-                    //Read response
-                    report(ByteSize.FromBytes(0), ByteSize.FromBytes(0), TransferState.ReadingResponse);
-                    try
-                    {
-                        response.EnsureSuccessStatusCode();
-                    }
-                    catch (HttpRequestException)
-                    {
-                        if (response.StatusCode == HttpStatusCode.NotFound)
-                            LogTo.Warning("Downloading '{url}' failed, server responded with 404 (NotFound)'",
-                                source);
-                        throw;
-                    }
-
-                    if (expectedLength != 0)
-                        if (response.Content.Headers.ContentLength > 0)
-                            if (Convert.ToUInt64(response.Content.Headers.ContentLength) != expectedLength)
-                                throw new HttpRequestException(
-                                    $"Size of the download ({response.Content.Headers.ContentLength}) differed from expected ({expectedLength})");
-                    var length = ByteSize.FromBytes(response.Content.Headers.ContentLength ?? 0);
-
-                    report(length, ByteSize.FromBytes(0), TransferState.ReadingResponse);
-                    var totalRead = 0;
-                    using (var input = await response.Content.ReadAsStreamAsync())
-                    {
-                        var buffer = new byte[16 * 1024];
-                        var read = 0;
-
-                        //Download
-                        report(length, ByteSize.FromBytes(0), TransferState.Downloading);
-                        while ((read = await input.ReadAsync(buffer, 0, buffer.Length, cancel)) > 0)
-                        {
-                            destination.Write(buffer, 0, read);
-                            totalRead += read;
-                            report(length, totalRead.Bytes(), TransferState.Downloading);
-                        }
-                    }
-
-                    //Finish
-                    report(length, totalRead.Bytes(), TransferState.Finished);
-                    observer.OnCompleted();
+                    response.EnsureSuccessStatusCode();
                 }
-                catch (Exception e)
+                catch (HttpRequestException)
                 {
-                    //Debugger.Break(); some downloads are immediatelly cancelled
-                    observer.OnError(e);
+                    if (response.StatusCode == HttpStatusCode.NotFound)
+                        LogTo.Warning("Downloading '{url}' failed, server responded with 404 (NotFound)'",
+                            source);
+                    throw;
                 }
+
+                if (expectedLength != 0)
+                    if (response.Content.Headers.ContentLength > 0)
+                        if (Convert.ToUInt64(response.Content.Headers.ContentLength) != expectedLength)
+                            throw new HttpRequestException(
+                                $"Size of the download ({response.Content.Headers.ContentLength}) differed from expected ({expectedLength})");
+                var length = ByteSize.FromBytes(response.Content.Headers.ContentLength ?? 0);
+
+                report(length, ByteSize.FromBytes(0), TransferState.ReadingResponse);
+
+                var totalRead = 0;
+                using (var input = await response.Content.ReadAsStreamAsync())
+                {
+                    var buffer = new byte[16 * 1024];
+                    var read = 0;
+
+                    //Download
+                    report(length, ByteSize.FromBytes(0), TransferState.Downloading);
+                    while ((read = await input.ReadAsync(buffer, 0, buffer.Length, cancel)) > 0)
+                    {
+                        destination.Write(buffer, 0, read);
+                        totalRead += read;
+                        report(length, totalRead.Bytes(), TransferState.Downloading);
+                    }
+                }
+
+                //Finish
+                report(length, totalRead.Bytes(), TransferState.Finished);
+                observer.OnCompleted();
 
                 return Disposable.Empty;
             }).SubscribeOn(RxApp.TaskpoolScheduler).Publish();
