0a12a2d6934b92e19f7a3722f2cb08dbf4a4aa32
J2ghz <j2.00ghz@gmail.com>
2018-09-15 15:30:18 +0200

    some experiments

---
+++ src/ModSink.Common.FSharp/RepoUtilities.fs
@@ -0,0 +1,20 @@
+﻿module RepoUtilities
+
+open System
+open DynamicData
+open DynamicDataEx
+
+let getFile (repo:Repo) = repo.Files
+
+let getFileSignature (file:RelativeFile) = file.Signature
+
+let getOnlineFiles (repos:IObservable<IChangeSet<Repo,Uri>>) =
+    repos
+    |> transformMany getFile getFileSignature
+
+let getDownloads (modpacks:IObservable<IChangeSet<Modpack,Guid>>) =
+    modpacks
+//  |> autoRefresh (fun modpack -> modpack.Selected)
+    |> filter (``return`` (fun modpack->modpack.Selected)) ()
+    |> transformMany (fun modpack->modpack.Mods) (fun _ -> Guid.NewGuid())
+    |> transformMany (fun modentry-> modentry.Mod.Files) (fun file -> file.Signature)
