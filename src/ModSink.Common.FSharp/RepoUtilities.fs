d1149f7ed4cda1ccaa89f1be9d8d4a8c9320e250
J2ghz <j2.00ghz@gmail.com>
2018-09-30 16:20:23 +0200

    remove old f#

--- src/ModSink.Common.FSharp/RepoUtilities.fs
+++ src/ModSink.Common.FSharp/RepoUtilities.fs
@@ -1,18 +1,3 @@
 module RepoUtilities
 
-open System
-open DynamicData
-open DynamicDataEx
-
-let getFile (repo : Repo) = repo.Files
-let getFileSignature (file : RelativeFile) = file.Signature
-let getOnlineFiles (repos : IObservable<IChangeSet<Repo, Uri>>) =
-    repos |> transformMany getFile getFileSignature
-
-//let getDownloads (modpacks : IObservable<IChangeSet<Modpack, Guid>>) =
-//    modpacks
-//    //  |> autoRefresh (fun modpack -> modpack.Selected)
-//    |> filter (``return`` (fun modpack -> modpack.Selected)) ()
-//    |> transformMany (fun modpack -> modpack.Mods) (fun _ -> Guid.NewGuid())
-//    |> transformMany (fun modentry -> modentry.Mod.Files)
-//           (fun file -> file.Signature)
+let create path =
