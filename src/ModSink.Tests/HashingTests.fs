ce5395e2373a6144b6263a83b1ace5df37004e8d
J2ghz <j2.00ghz@gmail.com>
2018-10-02 13:17:59 +0200

    add hashing test

---
+++ src/ModSink.Tests/HashingTests.fs
@@ -0,0 +1,16 @@
+﻿module HashingTests
+
+open Xunit
+open FsCheck
+open System.IO
+
+[<Fact>]
+let ``Returns a valid hash``() =
+    let hash arr =
+        use stream = new MemoryStream(arr,false)
+        let result =
+            Hashing.getHash stream
+            |> Async.RunSynchronously
+        result.Length = (Array.length arr |> int64)
+
+    Check.QuickThrowOnFailure hash
