206ecaf76092463edb14fef36def34b0647647e1
J2ghz <j2.00ghz@gmail.com>
2018-10-05 19:06:56 +0200

    some more tests

---
+++ src/ModSink.Tests/RepoUtilitiesTests.fs
@@ -0,0 +1,14 @@
+﻿module RepoUtilitiesTests
+
+open RepoUtilities
+open Xunit
+open FsUnit.Xunit
+open System
+
+[<Fact>]
+let ``Returns directory name from path``() =
+    getDirectoryName "C:\\dir\\" |> should equal "dir"
+
+[<Fact>]
+let ``Returns list of files in subdirectories``() =
+    getFiles Environment.CurrentDirectory |> Seq.length |> should greaterThan 0
