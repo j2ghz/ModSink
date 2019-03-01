009a02c538b7805f8476501ab86d7cb33f861047
J2ghz <j2.00ghz@gmail.com>
2019-03-01 23:45:03 +0100

    split viewmodels

---
+++ src/ModSink.UI/Model/SettingsModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using DynamicData;
+using DynamicData.Binding;
+using ModSink.Common.Client;
+using ReactiveUI;
+
+namespace ModSink.WPF.Model
+{
+    public class SettingsModel : ReactiveObject
+    {
+        public SettingsModel(ClientService client)
+        {
+            Client = client;
+            client.GroupUrls.Connect().ObserveOn(RxApp.MainThreadScheduler)
+                .Bind(GroupUrls)
+                .Subscribe();
+
+            // Temporary to ease testing
+            RxApp.TaskpoolScheduler.Schedule(() =>
+                client.GroupUrls.AddOrUpdate(@"https://files.417rct.org/Swifty_repos/modsink/group.bin"));
+        }
+
+        public ClientService Client { get; }
+
+        public ObservableCollectionExtended<string> GroupUrls { get; } = new ObservableCollectionExtended<string>();
+    }
+}
---
+++ src/ModSink.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModSink.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
