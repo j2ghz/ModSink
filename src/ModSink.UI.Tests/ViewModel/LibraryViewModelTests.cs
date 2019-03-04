4108dce8cb637e8888f862cc616b7a9420b36882
J2ghz <j2.00ghz@gmail.com>
2019-03-04 10:16:15 +0100

    add test project

---
+++ src/ModSink.UI.Tests/ViewModel/LibraryViewModelTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.Reactive.Testing;
+using Xunit;
+
+namespace ModSink.UI.Tests.ViewModel
+{
+    public class LibraryViewModelTests
+    {
+        [Fact]
+        public void AddModpack()
+        {
+            ReactiveUI.Testing.TestUtils.With(new TestScheduler(), sched =>
+            {
+                
+            });
+        }
+    }
+}
