a3931db5722c06e22ad09d4921662ab36ca9eafd
J2ghz <j2.00ghz@gmail.com>
2019-06-07 11:54:31 +0200

    add test, multiformats, and c#8

---
+++ src/ModSink.Domain/Entities/Repo/FileSignature.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ModSink.Common.Models.DTO.Repo
+{
+    [Serializable]
+    [DebuggerDisplay("{Hash} | {Length}")]
+    public struct FileSignature : IEquatable<FileSignature>, IComparable<FileSignature>
+    {
+        public FileSignature(HashValue hash, ulong length)
+        {
+            Hash = hash;
+            Length = length;
+        }
+
+        public FileSignature(HashValue hash, long length)
+        {
+            Hash = hash;
+            Length = Convert.ToUInt64(length);
+        }
+
+        public HashValue Hash { get; set; }
+
+        /// <summary>
+        ///     The length of the file taken from <see cref="FileInfo" />, in bytes.
+        /// </summary>
+        public ulong Length { get; set; }
+
+        public bool Equals(FileSignature other)
+        {
+            return Hash.Equals(other.Hash) && Length == other.Length;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            return obj is FileSignature signature && Equals(signature);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Hash.GetHashCode() * 397) ^ Length.GetHashCode();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Hash.ToString()} - {Length.ToString().PadLeft(10)}";
+        }
+
+        public int CompareTo(FileSignature other)
+        {
+            var hash = Hash.CompareTo(other.Hash);
+            if (hash != 0) return hash;
+            return Length.CompareTo(other.Length);
+        }
+    }
+}
---
+++ src/ModSink.Domain/Entities/Repo/Repo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Multiformats.Hash;
+
+namespace ModSink.Domain.Entities.Repo
+{
+    public class Repo
+    {
+        public IDictionary<Multihash, Uri> Files { get; }
+    }
+}
