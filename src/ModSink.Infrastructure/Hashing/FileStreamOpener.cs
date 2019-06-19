6b21a8cdff75fe545afc2a73059a68067e205f4d
J2ghz <j2.00ghz@gmail.com>
2019-06-19 14:38:31 +0200

    first test and filestream mock

--- src/ModSink.Infrastructure/Hashing/FileStreamOpener.cs
+++ src/ModSink.Infrastructure/Hashing/FileStreamOpener.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.IO.Abstractions;
-using System.Text;
 using Microsoft.Extensions.Options;
 using ModSink.Application.Hashing;
 
@@ -10,21 +7,26 @@ namespace ModSink.Infrastructure.Hashing
 {
     public class FileStreamOpener : IFileOpener
     {
-        private readonly IOptionsMonitor<Options> _optionsMonitor;
+        private readonly IFileStreamFactory _fileStreamFactory;
+
+        private readonly IOptions<Options> _options;
+
+        public FileStreamOpener(IOptions<Options> options, IFileStreamFactory fileStreamFactory)
+        {
+            _options = options;
+            _fileStreamFactory = fileStreamFactory;
+        }
+
+        public Stream OpenRead(IFileInfo file)
+        {
+            return _fileStreamFactory.Create(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read,
+                _options.Value.BufferSize, _options.Value.UseAsync);
+        }
 
         public class Options
         {
-            public int BufferSize { get; set; }
-            public bool UseAsync { get; set; }
-        }
-
-        public FileStreamOpener(IOptionsMonitor<Options> optionsMonitor)
-        {
-            _optionsMonitor = optionsMonitor;
-        }
-        public Stream OpenRead(IFileInfo file)
-        {
-            return new FileStream(file.FullName,FileMode.Open,FileAccess.Read,FileShare.Read,_optionsMonitor.CurrentValue.BufferSize,_optionsMonitor.CurrentValue.UseAsync);
+            public int BufferSize { get; set; } = 4096;
+            public bool UseAsync { get; set; } = false;
         }
     }
 }
