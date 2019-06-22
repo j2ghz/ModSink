7ec5c990819bc73db9ce693e077b78521ab2a330
J2ghz <j2.00ghz@gmail.com>
2019-06-22 13:21:42 +0200

    fix test

--- src/ModSink.Infrastructure.Tests/RepoBuilders/ModsInRepoRootBuilderTests.cs
+++ src/ModSink.Infrastructure.Tests/RepoBuilders/ModsInRepoRootBuilderTests.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
-using ModSink.Application.Hashing;
-using ModSink.Domain.Entities.Repo;
+using Microsoft.Extensions.Options;
+using ModSink.Infrastructure.Hashing;
 using ModSink.Infrastructure.RepoBuilders;
-using Moq;
+using ModSink.Infrastructure.Tests.Hashing;
 using Xunit;
 
 namespace ModSink.Infrastructure.Tests.RepoBuilders
@@ -16,22 +17,26 @@ namespace ModSink.Infrastructure.Tests.RepoBuilders
         [Fact]
         public async Task WithoutConfig()
         {
-            var hashingService = new Mock<IHashingService>();
-            var builder = new ModsInRepoRootBuilder(hashingService.Object);
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
             {
                 {@"/repo/mod1/a.txt", new MockFileData("a")},
                 {@"/repo/mod1/b.txt", new MockFileData("b")},
                 {@"/repo/mod2/c.txt", new MockFileData("c")}
             });
+            var hf = new SHA256();
+            var hashingService = new HashingService(hf, Options.Create(new HashingService.Options()),
+                new FileStreamOpener(Options.Create(new FileStreamOpener.Options()),
+                    new MockFileStreamFactory(fileSystem)));
+            var builder = new ModsInRepoRootBuilder(hashingService);
             var root = fileSystem.DirectoryInfo.FromDirectoryName(@"/repo/");
+
             var repo = await builder.Build(root,
                 CancellationToken.None);
-            repo.Name.Should().Be("repo");
-            repo.Files.Should().HaveCount(3)
-                ;
 
-            repo.Modpacks.Should().HaveCount(1).And.Contain(new Modpack());
+            repo.Name.Should().Be("repo");
+            repo.Files.Should().HaveCount(3);
+            repo.Modpacks.Should().HaveCount(1);
+            repo.Modpacks.Single().Mods.Select(m => m.Name).Should().Contain(new[] {"mod1", "mod2"});
         }
     }
 }
