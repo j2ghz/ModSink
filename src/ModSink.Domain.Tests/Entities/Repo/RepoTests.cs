318e24d2b5a769b34ba0fa0cdca0dc8870d62eb1
J2ghz <j2.00ghz@gmail.com>
2019-07-16 00:21:31 +0200

    Add repos should equal test

---
+++ src/ModSink.Domain.Tests/Entities/Repo/RepoTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ModSink.Domain.Tests.Entities.Repo
+{
+    public class RepoTests
+    {
+        [Fact]
+        public void RepoEquals()
+        {
+            var repo = new Domain.Entities.Repo.Repo();
+            var clone = new Domain.Entities.Repo.Repo();
+            repo.Should().Be(clone);
+            repo.Should().BeEquivalentTo(clone);
+        }
+    }
+}
