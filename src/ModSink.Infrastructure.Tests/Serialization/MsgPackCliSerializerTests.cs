561cb0c5c0cbbd4e23b265f5a2b5b277bf1162d3
J2ghz <j2.00ghz@gmail.com>
2019-09-12 12:14:56 +0200

    Add MessagePack

---
+++ src/ModSink.Infrastructure.Tests/Serialization/MsgPackCliSerializerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModSink.Application.Serialization;
+using ModSink.Application.Tests.Serialization;
+using ModSink.Infrastructure.Serialization;
+
+namespace ModSink.Infrastructure.Tests.Serialization
+{
+    public class MsgPackCliSerializerTests : IFormatterTests
+    {
+        protected override IFormatter formatter => new MsgPackCliSerializer();
+    }
+}
