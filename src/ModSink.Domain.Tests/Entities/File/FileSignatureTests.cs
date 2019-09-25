4f22738522f00b209c24c5ec383578abd93a7f82
J2ghz <j2.00ghz@gmail.com>
2019-09-25 23:46:50 +0200

    fix hash test

--- src/ModSink.Domain.Tests/Entities/File/FileSignatureTests.cs
+++ src/ModSink.Domain.Tests/Entities/File/FileSignatureTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
 using ModSink.Domain.Entities.File;
 using Xunit;
 
@@ -6,31 +9,37 @@ namespace ModSink.Domain.Tests.Entities.File
 {
     public class FileSignatureTests
     {
+        [Property]
+        public Property SameEqualsProperty(string id, byte[] value, long length)
+        {
+            return new Func<bool>(() => new FileSignature(new Hash(id, value), length) ==
+                    new FileSignature(new Hash($"{id}", value), length))
+                .When(!string.IsNullOrEmpty(id) && !(value is null) && value.Length > 0);
+        }
+
         [Fact]
         public void SameEquals()
         {
-            var fs1 =  new FileSignature(
-                    new Hash("",new byte[]
-                    {
-                        0xCA, 0x97, 0x81, 0x12, 0xCA, 0x1B, 0xBD, 0xCA, 0xFA, 0xC2, 0x31, 0xB3, 0x9A, 0x23, 0xDC,
-                        0x4D, 0xA7, 0x86, 0xEF, 0xF8, 0x14, 0x7C, 0x4E, 0x72, 0xB9, 0x80, 0x77, 0x85, 0xAF, 0xEE,
-                        0x48, 0xBB
-                    }),
-                    1L);
-
-            var fs2 = new FileSignature(
-                new Hash("",new byte[]
+            var fs1 = new FileSignature(
+                new Hash("test", new byte[]
                 {
                     0xCA, 0x97, 0x81, 0x12, 0xCA, 0x1B, 0xBD, 0xCA, 0xFA, 0xC2, 0x31, 0xB3, 0x9A, 0x23, 0xDC,
                     0x4D, 0xA7, 0x86, 0xEF, 0xF8, 0x14, 0x7C, 0x4E, 0x72, 0xB9, 0x80, 0x77, 0x85, 0xAF, 0xEE,
                     0x48, 0xBB
                 }),
                 1L);
-            
+
+            var fs2 = new FileSignature(
+                new Hash("test", new byte[]
+                {
+                    0xCA, 0x97, 0x81, 0x12, 0xCA, 0x1B, 0xBD, 0xCA, 0xFA, 0xC2, 0x31, 0xB3, 0x9A, 0x23, 0xDC,
+                    0x4D, 0xA7, 0x86, 0xEF, 0xF8, 0x14, 0x7C, 0x4E, 0x72, 0xB9, 0x80, 0x77, 0x85, 0xAF, 0xEE,
+                    0x48, 0xBB
+                }),
+                1L);
+
 
             fs1.Should().Be(fs2);
         }
-
-
     }
 }
