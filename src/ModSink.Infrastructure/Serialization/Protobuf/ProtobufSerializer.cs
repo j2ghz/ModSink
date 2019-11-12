ddcf7bce192e06333947b421cea48ad7177e6955
J2ghz <j2.00ghz@gmail.com>
2019-11-12 20:11:27 +0100

    Add protobuf boilerplate

---
+++ src/ModSink.Infrastructure/Serialization/Protobuf/ProtobufSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ModSink.Application.Serialization;
+
+namespace ModSink.Infrastructure.Serialization.Protobuf
+{
+    public class ProtobufSerializer : IFormatter
+    {
+        public Stream Serialize<T>(T o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize<T>(T o, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Deserialize<T>(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanDeserialize(string extension)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
