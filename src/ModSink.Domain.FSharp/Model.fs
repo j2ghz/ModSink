97d2fb773f0ea43ae4a9f33ee0b1a64cd5c210d1
J2ghz <j2.00ghz@gmail.com>
2019-11-17 00:12:22 +0100

    start f# model

---
+++ src/ModSink.Domain.FSharp/Model.fs
@@ -0,0 +1,37 @@
+﻿module Model
+
+open PathLib
+
+type RelativePath =
+    | RelativePath of p:IPurePath
+
+let c (p:IPurePath) = 
+    match p.IsAbsolute() with
+    | true -> Error ""
+    | false -> Ok (RelativePath p)
+
+type FileSignature = {
+    Hash: Hash;
+    Length: int64;
+}
+
+type Mod = {
+    Name: string;
+    Files: Map<RelativePath,FileSignature>
+}
+
+type Modpack = {
+    Name: string;
+    Mods: Mod Set;
+}
+
+type Hash = {
+    Algo: string;
+    Value: byte list
+}
+
+type Repo = {
+    Name: string;
+    Modpacks: Modpack Set;
+    Files: Map<FileSignature,IPurePath>;
+}
