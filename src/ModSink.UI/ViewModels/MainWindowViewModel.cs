70fa75a4be517f4a792575dacc98ff6871d10124
J2ghz <j2.00ghz@gmail.com>
2020-02-01 23:46:24 +0100

    add version info to main window

--- src/ModSink.UI/ViewModels/MainWindowViewModel.cs
+++ src/ModSink.UI/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace ModSink.UI.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        public string Title => $"{Assembly.GetEntryAssembly().GetName().Name} {Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion} on {RuntimeInformation.FrameworkDescription}";
+        public string Greeting => $"Hello World!";
     }
 }
