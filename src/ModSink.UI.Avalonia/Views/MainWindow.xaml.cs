075ad4120dd971462c9b4f635466f0dd342dce4b
J2ghz <j2.00ghz@gmail.com>
2020-02-01 20:17:12 +0100

    Add Avalonia project

---
+++ src/ModSink.UI.Avalonia/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ModSink.UI.Avalonia.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
